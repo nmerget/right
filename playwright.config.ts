fb203737a1b3a8a5a1b368040e192460b7af9a5b
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-06-03 22:02:48 +0200

    fix: issue with playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:5173/right",
+    baseURL: "http://localhost:5173/right",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -56,7 +56,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "pnpm run dev",
-    url: "http://127.0.0.1:5173",
+    port: 5173,
     reuseExistingServer: !process.env.CI,
   },
 });
