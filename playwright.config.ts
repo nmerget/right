6ae96769b8c0c0533774457e7f112c39d3b3c0f3
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-06-03 21:54:05 +0200

    fix: issue with playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -55,7 +55,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "pnpm run dev",
     url: "http://127.0.0.1:5173",
     reuseExistingServer: !process.env.CI,
   },
